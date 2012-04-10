0a33705aae34e721f2bc09608b95bfbe60d42c9c
Jay Collier <jay.collier@thecompass.com>
2012-04-10 11:30:27 -0400

    Changed permalink function text to [Read more]

--- wp-content/themes/maine-learning-home/functions.php
+++ wp-content/themes/maine-learning-home/functions.php
@@ -12,27 +12,27 @@ add_action( 'bp_core_activated_user', 'automatic_group_membership' );
 /* Add Cool Buttons to Activity Stream Items */
 /* http://bp-tricks.com/snippets/adding-buttons-to-activity-stream-entries/ */
  
-function my_bp_activity_entry_meta() {
+function my_bp_activity_entry_content() {
  
     if ( bp_get_activity_object_name() == 'blogs' && bp_get_activity_type() == 'new_blog_post' ) {?>
-        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">View Blog Post</a>
+        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">[Read more]</a>
     <?php }
  
     if ( bp_get_activity_object_name() == 'blogs' && bp_get_activity_type() == 'new_blog_comment' ) {?>
-        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">View Blog Comment</a>
+        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">[Read more]</a>
     <?php }
  
     if ( bp_get_activity_object_name() == 'activity' && bp_get_activity_type() == 'activity_update' ) {?>
-        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">View Activity Status</a>
+        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">[Read more]</a>
     <?php }
  
         if ( bp_get_activity_object_name() == 'groups' && bp_get_activity_type() == 'new_forum_topic' ) {?>
-        <a class="view-thread" href="<?php bp_activity_thread_permalink() ?>">View Discussion Thread</a>
+        <a class="view-thread" href="<?php bp_activity_thread_permalink() ?>">[Read more]</a>
     <?php }
  
         if ( bp_get_activity_object_name() == 'groups' && bp_get_activity_type() == 'new_forum_post' ) {?>
-        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">View Discussion Post</a>
+        <a class="view-post" href="<?php bp_activity_thread_permalink() ?>">[Read more]</a>
     <?php }
  
 }
-add_action('bp_activity_entry_meta', 'my_bp_activity_entry_meta');
+add_action('bp_activity_entry_content', 'my_bp_activity_entry_content');
