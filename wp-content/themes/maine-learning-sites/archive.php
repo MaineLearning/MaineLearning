4c358e61a2f86f5d0e0ba0aa1e73d2ddf8a85219
Jay Collier <jay.collier@thecompass.com>
2012-05-24 15:53:10 -0400

    Changed archive header to "Requested resources"

--- wp-content/themes/maine-learning-sites/archive.php
+++ wp-content/themes/maine-learning-sites/archive.php
@@ -8,14 +8,7 @@ function include_title() {
     global $wp_query; 
     if ( ! is_category() && ! is_tag() && ! is_tax() ) 
         return; 
-
-    $headline = $intro_text = ''; 
-    if ( single_cat_title("", false) ) 
-        $headline = sprintf( '<h1>%s</h1>', single_cat_title("", false) ); 
-    if ( category_description() ) 
-        $intro_text = category_description(); 
-    if ( $headline || $intro_text ) 
-        printf( '<div class="taxonomy-description">%s</div>', $headline . $intro_text );     
+        printf( '<div class="taxonomy-description"><h1>Requested resources</h1></div>', '');     
 };  
 
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
