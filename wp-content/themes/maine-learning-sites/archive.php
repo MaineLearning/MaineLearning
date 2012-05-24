a8613c24526edff7dd5cc3b0670de35140005e94
Jay Collier <jay.collier@thecompass.com>
2012-05-24 15:32:56 -0400

    Added title to category and taxonomy archives.

--- wp-content/themes/maine-learning-sites/archive.php
+++ wp-content/themes/maine-learning-sites/archive.php
@@ -3,11 +3,24 @@
 Template Name: Object Archive
 */
 
+add_action('genesis_before_loop', 'include_title'); 
+function include_title() { 
+    global $wp_query; 
+    if ( ! is_category() && ! is_tag() && ! is_tax() ) 
+        return; 
+
+    $headline = $intro_text = ''; 
+    if ( single_cat_title("", false) ) 
+        $headline = sprintf( '<h1>%s</h1>', single_cat_title("", false) ); 
+    if ( category_description() ) 
+        $intro_text = category_description(); 
+    if ( $headline || $intro_text ) 
+        printf( '<div class="taxonomy-description">%s</div>', $headline . $intro_text );     
+};  
+
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
 remove_action( 'genesis_post_content', 'genesis_do_post_image' );
 remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
-
-
 
 add_action( 'genesis_post_content', 'object_fields' );
 function object_fields() {
