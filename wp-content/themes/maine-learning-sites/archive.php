75a4315a22eac93fbd4f64cf9e36e719037184db
Jay Collier <jay.collier@thecompass.com>
2012-05-25 09:38:24 -0400

    Changed archive page to use WP-Views GUI

--- wp-content/themes/maine-learning-sites/archive.php
+++ wp-content/themes/maine-learning-sites/archive.php
@@ -11,13 +11,10 @@ function include_title() {
         printf( '<div class="taxonomy-description"><h1>Requested resources</h1></div>', '');     
 };  
 
+remove_action('genesis_post_title', 'genesis_do_post_title');
 remove_action( 'genesis_before_post_content', 'genesis_post_info' );
-remove_action( 'genesis_post_content', 'genesis_do_post_image' );
+remove_action( 'genesis_post_content', 'genesis_do_post_content' );
+add_action( 'genesis_post_content', 'the_content' );  
 remove_action( 'genesis_after_post_content', 'genesis_post_meta' );
-
-add_action( 'genesis_post_content', 'object_fields' );
-function object_fields() {
-    genesis_custom_field( 'wpcf-abstract-excerpt' );
-}
 
 genesis();
