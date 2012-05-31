302c8c0b115ea74701755860c78d81e879b8bd9e
Boone Gorges <boonebgorges@gmail.com>
2012-05-30 22:56:54 -0400

    Adds bp-custom.php file, with filters for external blog post email notifications
    This changeset adds two filters to modify the email notification sent when
    a new external blog post is posted to a group activity stream. The first one
    modifies the 'action' parameter to add quotation marks appropriately. The
    second one modifies the 'content' parameter, by removing the extraneous 'View
    external post' text.
    
    These filters are loaded in bp-custom.php, so that they're present when
    external blog posts are pulled into the activity stream via AJAX or cron job.

---
+++ wp-content/plugins/bp-custom.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Modifies the "action" string of external blog email notifications by adding quotation marks
+ * around post titles, blog titles, and group names
+ */
+function mainelearning_external_blog_notification_action( $action, $activity ) {
+	if ( 'exb' != $activity->type ) {
+		return $action;
+	}
+
+	$action = preg_replace( '/external post (.*?) from the blog (.*?) in the group (.*?)$/', 'external post "$1" from the blog "$2" in the group "$3"', $action );
+
+	return $action;
+}
+add_filter( 'bp_ass_activity_notification_action', 'mainelearning_external_blog_notification_action', 10, 2 );
+
+/**
+ * Modifies the "content" string of external blog email notifications by removing the 'View
+ * external post' string from the end
+ */
+function mainelearning_external_blog_notification_content( $content, $activity ) {
+	if ( 'exb' != $activity->type ) {
+		return $action;
+	}
+
+	$content = str_replace( 'View external post', '', $content );
+
+	return $content;
+}
+add_filter( 'bp_ass_activity_notification_content', 'mainelearning_external_blog_notification_content', 10, 2 );
+
+?>
