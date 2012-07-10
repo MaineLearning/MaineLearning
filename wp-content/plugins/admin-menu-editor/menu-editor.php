f342b3850033e5cdac66e16c1f9510058f7143da
Jay Collier <jay.collier@thecompass.com>
2012-07-10 14:16:45 -0400

    Updated to admin-menu-editor.1.1.9

--- wp-content/plugins/admin-menu-editor/menu-editor.php
+++ wp-content/plugins/admin-menu-editor/menu-editor.php
@@ -3,17 +3,16 @@
 Plugin Name: Admin Menu Editor
 Plugin URI: http://w-shadow.com/blog/2008/12/20/admin-menu-editor-for-wordpress/
 Description: Lets you directly edit the WordPress admin menu. You can re-order, hide or rename existing menus, add custom menus and more. 
-Version: 1.1.6.1
+Version: 1.1.9
 Author: Janis Elsts
-Author URI: http://w-shadow.com/blog/
+Author URI: http://w-shadow.com/
 */
 
 //Are we running in the Dashboard?
 if ( is_admin() ) {
 
     //Load the plugin
-    require 'includes/menu-editor-core.php';
+    require dirname(__FILE__) . '/includes/menu-editor-core.php';
     $wp_menu_editor = new WPMenuEditor(__FILE__, 'ws_menu_editor');
 
 }//is_admin()
-?>
