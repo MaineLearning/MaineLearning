09a8635d1e900d7ff504977a67c439547aac04cf
Jay Collier <jay.collier@thecompass.com>
2012-07-10 13:38:26 -0400

    Upgraded to hide-broken-shortcodes.1.5

--- wp-content/plugins/hide-broken-shortcodes/hide-broken-shortcodes.php
+++ wp-content/plugins/hide-broken-shortcodes/hide-broken-shortcodes.php
@@ -2,17 +2,19 @@
 /**
  * @package Hide_Broken_Shortcodes
  * @author Scott Reilly
- * @version 1.4
+ * @version 1.5
  */
 /*
 Plugin Name: Hide Broken Shortcodes
-Version: 1.4
+Version: 1.5
 Plugin URI: http://coffee2code.com/wp-plugins/hide-broken-shortcodes/
 Author: Scott Reilly
-Author URI: http://coffee2code.com
+Author URI: http://coffee2code.com/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Description: Prevent broken shortcodes from appearing in posts and pages.
 
-Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+, 3.1+, 3.2+, 3.3+.
+Compatible with WordPress 2.5 through 3.4+.
 
 =>> Read the accompanying readme.txt file for instructions and documentation.
 =>> Also, visit the plugin's homepage for additional information and updates.
@@ -24,19 +26,21 @@ TODO:
 */
 
 /*
-Copyright (c) 2009-2012 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2009-2012 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 if ( ! class_exists( 'c2c_HideBrokenShortcodes' ) ) :
@@ -45,9 +49,11 @@ class c2c_HideBrokenShortcodes {
 
 	/**
 	 * Returns version of the plugin.
+	 *
+	 * @since 1.4
 	 */
 	public static function version() {
-		return '1.4';
+		return '1.5';
 	}
 
 	/**
@@ -137,7 +143,7 @@ class c2c_HideBrokenShortcodes {
 			return substr( $m[0], 1, -1 );
 
 		// If text is being wrapped by opening and closing shortcode tag, show text. Otherwise, show nothing.
-		$default_display = ( isset( $m[5] ) ? $m[5] : '' );
+		$default_display = ( isset( $m[5] ) ? self::do_shortcode( $m[5] ) : '' );
 
 		// The filter is sending these arguments; apply_filters('hide_broken_shortcode', $default_display, $shortcode_name, $match_array)
 		return apply_filters( 'hide_broken_shortcode', $default_display, $m[2], $m );
@@ -148,5 +154,3 @@ class c2c_HideBrokenShortcodes {
 c2c_HideBrokenShortcodes::init();
 
 endif; // end if !class_exists()
-
-?>
